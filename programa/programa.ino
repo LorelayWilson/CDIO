75faf0ee71ff784079a9083d895d20e4955e5cfd
Lorena Florescu <lorelaywilson@gmail.com>
2019-10-27 11:13:32 +0100

    Programa acabado

--- programa/programa.ino
+++ programa/programa.ino
@@ -11,6 +11,7 @@ const int MaxSalineValue = 20873; //Valor de la medida del sensor en agua destil
 const int H = 600; //constante que usaremos como margen de error para humedad
 const int S = 500; //constante que usaremos como margen de error para salinidad
 
+//Función que se produce una sola vez para ajustar lo que necesitemos en el programa
 void setup() {
   Serial.begin(9600); //Establecemos la velocidad de datos en 9600 baudios
   pinMode(pin_sal, OUTPUT); //Configuramos el pin 5 como salida
@@ -19,36 +20,44 @@ void setup() {
   
 }
 
+//Función loop donde se llamará a las funciones de los sensores
 void loop() {
-  //Muestra los valores de salinidad y humedad en porcentaje
+  //Muestra los valores de salinidad y humedad en porcentaje llamando a las funciones creadas para ello
   medirHumedad();
   medirSalinidad();
   Serial.println("");
   delay (5000);
 }
 
+//Función de humedad para calcular y mostrar por pantalla el porcentaje de humedad
 void medirHumedad(){
 
-  int16_t humedad;
+  //Inicializamos las variables donde guardaremos el porcentaje de humedad y la lectura del sensor
+  int16_t humedad, adc0;
+  
+  //Guardamos el valor que mide el sensor de humedad que se comunica con el pin A0
+  adc0 = ads1115.readADC_SingleEnded(0);
+   
+  //Guardamos en humedad el calculo del porcentaje
   humedad = 100*AirValue/(AirValue-WaterValue)-adc0*100/(AirValue-WaterValue);
 
-     if(adc0<(WaterValue - H) or adc0>(AirValue + H)) {
-      Serial.print("Error lectura humedad: ");
-      Serial.print(humedad);
-      Serial.print("%   -   ");
-      Serial.println(adc0);
-    } else{
-      Serial.print("La humedad es: ");
-      Serial.print(humedad);
-      Serial.print("%   -   ");
-      Serial.println(adc0);
+  //Bucle que usamos para mandar mensaje de error si la lectura del adc0 no concuerda con la calibración
+  if(adc0<(WaterValue - H) or adc0>(AirValue + H)) {
+    Serial.print("Error lectura humedad: ");
+    Serial.print(humedad);
+    Serial.println("%");
+  } else{
+    Serial.print("La humedad es: ");
+    Serial.print(humedad);
+    Serial.println("%");
     }
 }
 //Descripción de la función: muestra por pantalla el porcentaje de salinidad
 void medirSalinidad(){
 
+  //Inicializamos variables donde guardaremos el porcentaje de salinidad y la lectura del sensor
   int16_t salinidad, adc1;
-
+  
   digitalWrite( pin_sal, HIGH ); 
   delay(100); 
 
@@ -56,10 +65,6 @@ void medirSalinidad(){
   adc1 = ads1115.readADC_SingleEnded(1); 
   digitalWrite( pin_sal, LOW ); 
   delay(10); 
-
-  //Mostramos el valor que se obtiene directamente del sensor en el pin A1
-  Serial.print("Adc1: ");
-  Serial.println(adc1);
 
   //Aplicamos la siguiente fórmula para calcular el porcentaje de salinidad
   salinidad = ((adc1 - NoSalineValue)* 100)/(MaxSalineValue - NoSalineValue);
