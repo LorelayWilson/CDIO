73689b057133be19c37702fa1e252d3ac12bcadd
Lorena Florescu <lorelaywilson@gmail.com>
2019-11-17 22:44:55 +0100

    corrección fallo lecturas

--- libreries/Sensor/Sensor.cpp
+++ libreries/Sensor/Sensor.cpp
@@ -4,40 +4,19 @@
 
 #include "Sensor.h"
 
-// --------------- CONSTRUCTORES -------------
-
-Sensor::Sensor(){
-
-}
-
-Sensor::Sensor(int pin, double min_valor, double max_valor, Adafruit_ADS1115 ads){
-  pinADC = pin;
-  valor_min = min_valor;
-  valor_max = max_valor;
-  ads1115 = ads;
-  pin = 0;
-  lectura = 0;
-}
-
-Sensor::Sensor(int pin, double min_valor, double max_valor, int pinC, Adafruit_ADS1115 ads){
-  pinADC = pin;
-  min_valor = min_valor;
-  max_valor = max_valor;
-  pin = pinC;
-  ads1115 = ads;
-  lectura = 0;
-}
-
-
 // -------------- FUNCIONES PARA MOSTRAR POR PANTALLA --------------
 
-//Función para humedad
-void Sensor::lecturaHumedad(){
+//Función de humedad para calcular y mostrar por pantalla el porcentaje de humedad
+void medirHumedad(int airValue, int waterValue, int16_t adc) {
 
-  lectura = ads1115.readADC_SingleEnded(pinADC);
-  int16_t humedad = 100 * valor_min / (valor_min  - valor_max) - lectura * 100 / (valor_min - valor_max);
+  int H = 600; //constante que usaremos como margen de error para humedad
+  //Creamos las variables donde guardaremos el porcentaje de humedad y la lectura del sensor
+  
+  //Guardamos en humedad el calculo del porcentaje
+  int16_t humedad = 100 * airValue / (airValue - waterValue) - adc * 100 / (airValue - waterValue);
+
   //Bucle que usamos para mandar mensaje de error si la lectura del adc0 no concuerda con la calibración
-  if (lectura < (valor_max - 500) or lectura > (valor_min + 500)) {
+  if (adc < (waterValue - H) or adc > (airValue + H)) {
     Serial.print("Error lectura humedad: ");
     Serial.print(humedad);
     Serial.println("%");
@@ -46,23 +25,22 @@ void Sensor::lecturaHumedad(){
     Serial.print(humedad);
     Serial.println("%");
   }
-  
 }
 
-//Función para salinidad
 
-void Sensor::lecturaSalinidad(){
+//Descripción de la función: muestra por pantalla el porcentaje de salinidad
+void medirSalinidad(int noSalineValue, int maxSalineValue, int16_t adc) {
 
-  digitalWrite(pin, HIGH); 
-  delay(100); 
-  lectura = ads1115.readADC_SingleEnded(pinADC);
-  digitalWrite( pin_sal, LOW ); 
-  delay(10); 
+  int S = 500; //constante que usaremos como margen de error para salinidad
+
+  //Creamos variables donde guardaremos el porcentaje de salinidad y la lectura del sensor
+  int16_t salinidad;
+
   //Aplicamos la siguiente fórmula para calcular el porcentaje de salinidad
-  int16_t salinidad = ((lectura - valor_min)* 100)/(valor_max - valor_min);
+  salinidad = ((adc - noSalineValue) * 100) / (maxSalineValue - noSalineValue);
 
- //Filtramos los valores obtenidos del sensor para que ver si se encuentran dentro de los límites de la calibración
-  if (lectura < (valor_min - 500) or lectura > (valor_max + 500)) {
+  //Filtramos los valores obtenidos del sensor para que ver si se encuentran dentro de los límites de la calibración
+  if (adc < (noSalineValue - S) or adc > (maxSalineValue + S)) {
     Serial.print("Error de lectura salinidad: ");
     Serial.print("La salinidad es: ");
     Serial.print(salinidad);
@@ -73,21 +51,20 @@ void Sensor::lecturaSalinidad(){
     Serial.print(salinidad);
     Serial.println(" %");
   }
-  
 }
 
-//Funcion para temperatura
 
-void Sensor::lecturaTemperatura(){
+void medirTemperatura(int ordenada_calibrado, int pendiente_calibrado, int16_t adc) {
 
-  lectura = ads1115.readADC_SingleEnded(pinADC);
+  //Creamos las variables donde guardaremos el porcentaje d y la lectura del sensor
+  int16_t temperatura, voltaje;
 
-  int16_t voltaje = (4096 / (pow(2, 15) - 1)) * lectura;
-  Serial.println(lectura);
-  Serial.println(voltaje);
+  //Guardamos el valor que mide el sensor de temperatura que se comunica con el pin A2
+
+  voltaje = (4096 / (pow(2, 15) - 1)) * adc;
+
   //Guardamos en humedad el calculo del porcentaje
-  double temperatura = (voltaje - valor_max) / valor_min;
-  //Bucle que usamos para mandar mensaje
-  Serial.print("Temperatura es: ");
+  temperatura = (voltaje - ordenada_calibrado) / pendiente_calibrado;
+  Serial.print("Temperatura: ");
   Serial.println(temperatura);
 }
--- libreries/Sensor/Sensor.h
+++ libreries/Sensor/Sensor.h
@@ -6,24 +6,11 @@
  #define SENSOR_h
 
  #include "Arduino.h"
-#include <Adafruit_ADS1015.h>
 
- class Sensor{
 
-    private:
-    	int pinADC;
-    	double valor_min;
-    	double valor_max;
-    	int lectura;
-    	int pin;
-    	Adafruit_ADS1115 ads1115;
 
-    public:
-    	Sensor();
-    	Sensor(int, double, double, Adafruit_ADS1115);
-    	Sensor(int, double, double, int, Adafruit_ADS1115);
-    	void lecturaHumedad();
-    	void lecturaSalinidad();
-    	void lecturaTemperatura(); 
- };
+void medirHumedad(int, int, int16_t);
+void medirSalinidad(int, int, int16_t);
+void medirTemperatura(int, int, int16_t); 
+
  #endif
